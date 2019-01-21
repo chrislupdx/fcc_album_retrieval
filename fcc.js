ca7199edea376fdc3fa71b1abb8428801f4d2360
cl <christopherlu.pdx@gmail.com>
2019-01-20 16:54:23 -0800

    added first section

---
+++ fcc.js
@@ -0,0 +1,25 @@
+function updateRecords(id, prop, value) {
+
+if (prop != "tracks", value == []) {
+  collection[id].album  = value;
+  return collection
+}
+
+else if (prop == "tracks" && album["tracks"] != null){
+  // expressing if tracks does not exist
+  collection[id].
+  create an empty array before adding the new value to the album's corresponding property
+}
+
+else if (prop == "tracks" && value != "" ) {
+  array.push(value)
+  push value to the end of the album's list track
+  return collection
+}
+
+else if (value = "") {
+    delete collection.prop;
+    return collection
+}
+
+}
